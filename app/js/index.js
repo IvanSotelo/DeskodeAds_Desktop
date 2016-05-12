18ef63a208a0a4467f04e060657ca513d1183adb
Ivan Sotelo <ivan-.-sotelo@hotmail.com>
2016-05-12 16:13:21 -0600

    Actualizacion

--- app/js/index.js
+++ app/js/index.js
@@ -8,69 +8,107 @@
 var ipc = require('ipc');
 var configuration = require('../configuration');
 
-if (!configuration.readSettings('shortcutKeys')) {
-    alert('Hola');
-}
+if (!configuration.readSettings('pantalla-Id')) {
+    $('.light-box').show();
+    $('.form-box').show();
+    $('.form-group input').on('focus blur', function (e) {
+        $(this).parents('.form-group').toggleClass('active', (e.type === 'focus' || this.value.length > 0));
+    }).trigger('blur');
+    $('.form-group select').on('focus blur', function (e) {
+        $(this).parents('.form-group').toggleClass('active', (e.type === 'focus' || this.value.length > 0));
+    }).trigger('blur');
 
-var myPlayer,
-  eVideoName = document.getElementById("videoName"),
-  eTimeRemaining = document.getElementById("timeRemaining"),
-  timeRemaining,
-  totalTime,
-  currentVideoIndex = 0,
-  newVideo,
-  firstVideo = true,
-  playlistData = [{
-    "name": "Great Blue Heron",
-    "thumbnailURL": "//solutions.brightcove.com/bcls/assets/images/Great-Blue-Heron.png",
-    "sources": [{
-      "type": "application/x-mpegURL",
-      "src": "http://solutions.brightcove.com/bcls/assets/videos/Great-Blue-Heron.m3u8"
-    }, {
-      "type": "video/mp4",
-      "src": "http://solutions.brightcove.com/bcls/assets/videos/Great-Blue-Heron.mp4"
-    }]
-  }, {
-    "name": "Birds of a Feather",
-    "thumbnailURL": "http://solutions.brightcove.com/bcls/assets/images/BirdsOfAFeather.png",
-    "sources": [{
-      "type": "video/mp4",
-      "src": "http://solutions.brightcove.com/bcls/assets/videos/BirdsOfAFeather.mp4"
-    }]
-  }, {
-    "name": "Sea Marvels",
-    "thumbnailURL": "http://solutions.brightcove.com/bcls/assets/images/Sea Marvels.png",
-    "sources": [{
-      "type": "video/mp4",
-      "src": "http://solutions.brightcove.com/bcls/assets/videos/Sea-Marvels.mp4"
-    }]
-  }];
-function loadVideo() {
-  if (currentVideoIndex < playlistData.length) {
-    // load the new video
-    myPlayer.src(playlistData[currentVideoIndex].sources);
-
-    // increment the current video index
-    currentVideoIndex++;
-    // play the video
-    if (firstVideo) {
-      firstVideo = false;
-    } else {
-      myPlayer.play();
+    get_loc();
+    function get_loc() {
+           if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(coordenadas);
+           }else{
+              alert('Este navegador es algo antiguo, actualiza para usar el API de localización');                  }
     }
-  }else {
-    currentVideoIndex = 0;
-    loadVideo();
-  }
-};
-videojs("video_1").ready(function () {
-  myPlayer = this;
-  myPlayer.on("ended", function () {
-    loadVideo();
-  });
-  myPlayer.on("timeupdate", function (evt) {
 
+    function coordenadas(position) {
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
+    }
+
+
+        configuration.saveSettings('pantalla-Id', '42');
+alert(configuration.readSettings('pantalla-Id'));
+}else {
+
+
+  $(window).load(function(){
+      $(".fakeloader").fakeLoader({
+          timeToHide:12200,
+          bgColor:"#1A1A20",
+          imagePath:"img/load1.gif",
+      });
+      $.jdeskodeads.getPantallaVideos(configuration.readSettings('pantalla-Id'), function(Pantalla){
+          var playlistData = JSON.stringify(Pantalla.videos);
+        });
   });
-  // load the first video
-  loadVideo();
-});
+
+
+  var myPlayer,
+    eVideoName = document.getElementById("videoName"),
+    eTimeRemaining = document.getElementById("timeRemaining"),
+    timeRemaining,
+    totalTime,
+    currentVideoIndex = 0,
+    newVideo,
+    firstVideo = true,
+    playlistData = [{
+      "name": "Great Blue Heron",
+      "thumbnailURL": "//solutions.brightcove.com/bcls/assets/images/Great-Blue-Heron.png",
+      "sources": [{
+        "type": "application/x-mpegURL",
+        "src": "video/1.mp4"
+      }, {
+        "type": "video/mp4",
+        "src": "video/1.mp4"
+      }]
+    }, {
+      "name": "Birds of a Feather",
+      "thumbnailURL": "http://solutions.brightcove.com/bcls/assets/images/BirdsOfAFeather.png",
+      "sources": [{
+        "type": "video/mp4",
+        "src": "http://solutions.brightcove.com/bcls/assets/videos/BirdsOfAFeather.mp4"
+      }]
+    }, {
+      "name": "Sea Marvels",
+      "thumbnailURL": "http://solutions.brightcove.com/bcls/assets/images/Sea Marvels.png",
+      "sources": [{
+        "type": "video/mp4",
+        "src": "http://solutions.brightcove.com/bcls/assets/videos/Sea-Marvels.mp4"
+      }]
+    }];
+  function loadVideo() {
+    if (currentVideoIndex < playlistData.length) {
+      // load the new video
+      myPlayer.src(playlistData[currentVideoIndex].sources);
+
+      // increment the current video index
+      currentVideoIndex++;
+      // play the video
+      if (firstVideo) {
+        firstVideo = false;
+      } else {
+        myPlayer.play();
+      }
+    }else {
+      currentVideoIndex = 0;
+      loadVideo();
+    }
+  };
+  videojs("video_1").ready(function () {
+    myPlayer = this;
+    myPlayer.on("ended", function () {
+      loadVideo();
+    });
+    myPlayer.on("timeupdate", function (evt) {
+
+    });
+    // load the first video
+    loadVideo();
+  });
+}
--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
+var configuration = require('./configuration');
+var ipc = require('ipc');
 
 app.on('window-all-closed', function() {
   if(process.platform != 'darwin') {
@@ -13,6 +15,9 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
+  if (configuration.readSettings('pantalla-Id')) {
+
+  }
   mainWindow = new BrowserWindow({
     fullscreen:true,
     });
@@ -21,4 +26,35 @@ app.on('ready', function() {
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+});
+
+
+function setGlobalInfo() {
+    globalInfo.unregisterAll();
+
+    var IdKey = configuration.readSettings('pantalla-Id');
+    var CategoriaKey = configuration.readSettings('pantalla-Categoria');
+    var UbicacionKey = configuration.readSettings('pantalla-Ubicacion');
+    var LatKey = configuration.readSettings('pantalla-Lat');
+    var LngKey = configuration.readSettings('pantalla-Lng');
+
+    globalInfo.register(IdKey, function () {
+        mainWindow.webContents.send('global-info', 0);
+    });
+    globalInfo.register(CategoriaKey, function () {
+        mainWindow.webContents.send('global-info', 1);
+    });
+    globalInfo.register(UbicacionKey, function () {
+        mainWindow.webContents.send('global-info', 2);
+    });
+    globalInfo.register(LatKey, function () {
+        mainWindow.webContents.send('global-info', 3);
+    });
+    globalInfo.register(LngKey, function () {
+        mainWindow.webContents.send('global-info', 4);
+    });
+}
+
+ipc.on('set-global-Info', function () {
+    setGlobalInfo();
 });
